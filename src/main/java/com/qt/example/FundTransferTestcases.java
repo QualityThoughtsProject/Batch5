b3dd6985167f3f0637013535e15564c3eb18b2af
NagaDurgaRao <nagadurgaraochintalapudi97@gmail.com>
2023-11-10 11:20:37 +0530

    added the fund transfer

--- src/main/java/com/qt/example/FundTransferTestcases.java
+++ src/main/java/com/qt/example/FundTransferTestcases.java
@@ -5,7 +5,17 @@ import org.testng.annotations.Test;
 public class FundTransferTestcases {
 
     @Test
-    public void transferFund(){
+    public void transferFund1(){
+        System.out.println("transferFund1");
+
+    }
+
+    public void transferFund2(){
+        System.out.println("transferFund2");
+
+    }
+    public void transferFund3(){
+        System.out.println("transferFund3");
 
     }
 }
